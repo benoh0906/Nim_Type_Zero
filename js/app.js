db046df76dd1c8f3fc8dcde547fbb7fbeb7ea8b6
Jungbin Oh <benoh0906@gmail.com>
2019-07-05 13:59:04 -0700

    create mthods for shuffling and distributing cards

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+
 class Player {
     constructor(name){
         this.name=name;
@@ -17,6 +22,9 @@ const game = {
     cumulative: 0,
     players: [],
     cards:[],
+    gilbreathDeck:[],
+    finalDeck:[],
+    
 
 
     playersCreate () {
@@ -29,13 +37,61 @@ const game = {
     cardsCreate() {
 
         for( let j = 0; j <4; j++){
-            for( let i = 0; i<10; i++){
+
+            
+            for( let i = 0; i<6; i++){
                 this.cards.push(new Card(j))
             }
         }
         console.log(this.cards)
     },
+
+    gilbreathCreate(){
+
+        for( let j = 0; j <4; j++){
+            
+            for( let i = 0; i<4; i++){
+                this.gilbreathDeck.push(new Card(j))
+            }
+        }
+        console.log(this.gilbreathDeck)
+    },
+
+    gilbreathShuffle(){
+        let shuffleGil = shuffle(this.gilbreathDeck)
+        let shuffleCard= shuffle(this.cards)
+        this.finalDeck=this.gilbreathDeck.concat(this.cards)
+        // this.finalDeck.push(shuffle(this.gilbreathDeck))
+        // this.finalDeck.push(shuffle(this.cards))
+        console.log(this.finalDeck)
+    },
+
+    distributeCards () {
+
+        for (let i = 0; i < 16; i++){
+            if(i>=0 && i<4){
+                this.players[0].hand.push(this.finalDeck[i].value)
+            } else if(i>=4 && i<8){
+                this.players[1].hand.push(this.finalDeck[i].value)
+            } else if(i>=8 && i<12){
+                this.players[2].hand.push(this.finalDeck[i].value)
+            } else {
+                this.players[3].hand.push(this.finalDeck[i].value)
+            }
+        }
+        console.log(this.players)
+    },
+
+    playCard() {
     
+    }
+
+
+    
+
+
+
+
 
     
 
@@ -43,3 +99,6 @@ const game = {
 
 game.playersCreate()
 game.cardsCreate()
+game.gilbreathCreate()
+game.gilbreathShuffle()
+game.distributeCards()
